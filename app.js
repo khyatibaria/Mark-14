36d1a5307a034c58d0be5a30c0f569a087e69461
Khyati Baria <67405584+khyatibaria@users.noreply.github.com>
2021-09-16 23:29:25 +0530

    Update style.css

--- app.js
+++ app.js
@@ -9,22 +9,31 @@ function calculateProfitAndLoss( initial, quantity, current){
     var initial=initialPrice.value;
     var quantity=quantityofStock.value;
     var current= currentPrice.value;
+    if(initial>0 && quantity >0 && current>0){
+        if(initial > current){
+            var loss=(initial-current) * quantity;
+            var lossPercentage=(loss/initial)*100;
+            resultDiv.style.display = "block";
+            resultDiv.innerText=`Hey your Loss is ${loss} and Loss Percentage is ${lossPercentage}`;
+        }
+        else if(current > initial)
+        {
+            var profit =(current-initial)* quantity;
+            var profitPercentage=(profit/initial)*100;
+            resultDiv.style.display = "block";
+            resultDiv.innerText=`Hey your Profit is ${profit} and Profit Percentage is ${profitPercentage}`;
+        }
+        else{
+            resultDiv.style.display = "block";
+            resultDiv.innerText="No Pain No Gain";
+        }
 
-    if(initial > current){
-        var loss=(initial-current) * quantity;
-        var lossPercentage=(loss/initial)*100;
-        resultDiv.innerText=`Hey your Loss is ${loss} and Loss Percentage is ${lossPercentage}`;
-    }
-    else if(current > initial)
-    {
-        var profit =(current-initial)* quantity;
-        var profitPercentage=(profit/initial)*100;
-        resultDiv.innerText=`Hey your Profit is ${profit} and Profit Percentage is ${profitPercentage}`;
     }
     else{
-        resultDiv.innerText="No Pain No Gain";
+        resultDiv.style.display = "block";
+        resultDiv.innerText="Enter Positive Values";
     }
-    
+   
 }
 
 checkBtn.addEventListener('click',calculateProfitAndLoss)
